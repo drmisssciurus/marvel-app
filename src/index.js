22d10c90a07410ae4e67542602130b60102c3506
Alina <ms.sciurus@gmail.com>
2024-10-07 15:02:41 +0300

    data transform

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./components/app/App";
-import MarvelServise from "./services/MarvelService";
+
 import "./style/style.scss";
 
-const marvelService = new MarvelServise();
 
-marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)))
 
 
 const container = document.getElementById("root");
