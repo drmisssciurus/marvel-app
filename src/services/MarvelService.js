b3c92ab015683777c97c9a4e35f23f6b423f61c5
Alina <ms.sciurus@gmail.com>
2024-10-08 15:03:03 +0300

    live cycle practice

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,39 +1,41 @@
 
 
-class MarvelServise {
+class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=2d6a6b1701173f7e202c3079b740341d';
 
 
-    getResourse = async (url) => {
+    getResource = async (url) => {
         let res = await fetch(url);
-
-        if(!res.ok) {
+    
+        if (!res.ok) {
             throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
-
+    
         return await res.json();
     }
 
     getAllCharacters = async () => {
-        const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
     getCharacter = async (id) => {
-        const res = await this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
         return this._transformCharacter(res.data.results[0]);
     }
 
     _transformCharacter = (char) => {
         return {
+            id: char.id,
             name: char.name,
-            description: char.description,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items
         }
     }
 }
 
-export default MarvelServise;
+export default MarvelService;
---
+++ test.js
@@ -0,0 +1,19 @@
+
+
+// function makeStar(stars) {
+//     let result;
+//     console.log(result);
+
+//     result = '';
+//     for (let i = 0; i < stars; i++) {
+//         result += '★';
+//     }
+//     console.log(result);
+//     return result;
+    
+// }
+
+// console.log(makeStar(1));
+
+
+
